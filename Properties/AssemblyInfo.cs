2bf7b79de7962947799b792af21fc72095761e47
ViniLog <Devka.damax@bk.ru>
2025-02-06 20:36:18 +0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("InjectorV")]
+[assembly: AssemblyDescription("Free dll Injector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ViniLog")]
+[assembly: AssemblyProduct("InjectorV")]
+[assembly: AssemblyCopyright("Copyright © ViniLog  2025")]
+[assembly: AssemblyTrademark("InjectorV")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0426b552-4942-45a5-ab6d-4a3d535622e4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
